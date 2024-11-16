aa36e0b94cf33794b194e89661db4b3e4777fd43
BonBon <per_cherry@163.com>
2024-11-16 11:41:39 +0800

    屏蔽 pubhtml5 logo

--- src/components/pdf.tsx
+++ src/components/pdf.tsx
@@ -1,8 +1,11 @@
+import { useRef } from "react";
 import useApp from "../hooks/useApp";
 import useProject from "../hooks/useProject";
 import Close from "./close";
 
 export default function PDF() {
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+
   const { hidePDF } = useProject();
   const { width, height, hideMask } = useApp();
 
@@ -13,8 +16,11 @@ export default function PDF() {
 
   return (
     <div className="relative w-full h-full flex justify-center items-end z-[60]">
-      <Close onClick={handleClose} />
+      <div className="absolute top-0 left-0 w-full h-[40px] bg-black">
+        <Close onClick={handleClose} />
+      </div>
       <iframe
+        ref={iframeRef}
         style={{ width, height }}
         src="https://online.pubhtml5.com/ugrmb/zxjw/"
         seamless
